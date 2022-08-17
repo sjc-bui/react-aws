571af07b6825de595e3787ac13b5090b436ba211
sjc-bui <bui@setjapan.co.jp>
2022-08-17 10:57:26 +0900

    Update message.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App()
+{
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>TESTING GITHUB ACTIONS & S3</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
